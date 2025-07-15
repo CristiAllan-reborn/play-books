051a117cd3055e4a768eab01bc64e043866663fc
Cristi Mendonça <cristiallan@gmail.com>
2025-07-15 14:47:23 -0300

    install rspec and factory bot

---
+++ spec/examples/example_spec.rb
@@ -0,0 +1,20 @@
+# This is an example spec file to demonstrate RSpec usage
+# You can delete this file once you start writing your own specs
+
+RSpec.describe "Example" do
+  it "should pass" do
+    expect(true).to be true
+  end
+
+  it "should demonstrate basic math" do
+    expect(2 + 2).to eq(4)
+  end
+
+  # Example of using Factory Bot (uncomment when you have models)
+  # describe User do
+  #   it "should create a user with factory" do
+  #     user = create(:user)
+  #     expect(user).to be_valid
+  #   end
+  # end
+end
---
+++ spec/factories/application_record.rb
@@ -0,0 +1,8 @@
+# This file can be used as a template for creating factories
+# FactoryBot.define do
+#   factory :user do
+#     sequence(:email) { |n| "user#{n}@example.com" }
+#     password { "password123" }
+#     password_confirmation { "password123" }
+#   end
+# end
